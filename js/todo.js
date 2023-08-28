43dc681ba46d9d9e682ef645afc709e2e244fe83
AlejandroRG12 <garcia4312@hotmail.com>
2023-08-28 11:03:36 -0600

    terminamos por fin

--- js/todo.js
+++ js/todo.js
@@ -1,24 +1,66 @@
 const formulario = document.getElementById('formulario');
-const istaTareas = document.getElementById('lista-tareas');
+const listaTareas = document.getElementById('lista-tareas');
 const template = document.getElementById('template').content;
 const fragment = document.createDocumentFragment();
 
 //variables globales para las tareas como objeto
-let tarea = {};
+let tareas = {};
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('tareas')) {
-        tarea = JSON.parse(localStorage.getItem('tareas'));
+        tareas = JSON.parse(localStorage.getItem('tareas'));
     }
     pintarTareas();
 });
 
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+    setTarea(e);
+});
+
+listaTareas.addEventListener('click', e => {
+    btnAcciones(e);
+});
+
+const btnAcciones = e => {
+    if(e.target.classList.contains('fa-circle-check')){
+        tareas[e.target.dataset.id].estado = true;
+        pintarTareas();
+    }
+    if(e.target.classList.contains('fa-undo-alt')){
+        tareas[e.target.dataset.id].estado = false;
+        pintarTareas();
+    }
+    if(e.target.classList.contains('fa-circle-minus')){
+        delete tareas[e.target.dataset.id]
+        pintarTareas();
+    }
+}
+
+const setTarea = e => {
+    const texto = e.target.querySelector('input').value;
+    if(texto.trim() === ''){
+        alert('Tarea vacia');
+        return
+    }
+    const tarea = {
+        id: Date.now(),
+        texto,
+        estado: false
+    }
+    tareas[tarea.id] = tarea;
+    pintarTareas();
+    formulario.reset();
+    e.target.querySelector('input').focus();
+};
+
 const pintarTareas = () => {
+    console.log(tareas);
     localStorage.setItem('tareas', JSON.stringify(tareas));
     if (Object.values(tareas).length === 0){
-        istaTareas.innerHTML = `
-            <div class="alert alert-dark">
+        listaTareas.innerHTML = `
+            <div class="alert alert-dark d-flex justify-content-center">
                 NO TASK PENDING ⏰
             </div>
         `
@@ -30,12 +72,12 @@ const pintarTareas = () => {
         const clone = template.cloneNode(true);
         clone.querySelector('p').textContent = tarea.texto;
         if(tarea.estado) {
-            clone.querySelectorAll('.fas')[0].classList.replace('fa-circle-check', 'fa-undo-alt');
+            clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-circle-check', 'fa-undo-alt');
             clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary');
             clone.querySelector('p').style.textDecoration = 'line-through'
         }
-        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id;
-        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id;
+        clone.querySelectorAll('.fa-solid')[0].dataset.id = tarea.id;
+        clone.querySelectorAll('.fa-solid')[1].dataset.id = tarea.id;
         fragment.appendChild(clone);
     });
     listaTareas.appendChild(fragment);
